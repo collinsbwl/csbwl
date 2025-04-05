740be867abba6d9fb677d8ee7c0e294e05dea5af
Collin.S <collinsobo3@gmail.com>
2025-04-05 13:39:52 -0400

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,9 @@ import { ChakraProvider, Container, defaultSystem } from "@chakra-ui/react";
 import Header from "@/components/header";
 import AboutMe from "@/components/aboutme";
 import Projects from "@/components/projects";
-import Oldexp from "@/components/oldexp";
 import Experience from "@/components/experience";
+import Click from "@/components/ClickSpark";
+import { CardSpotlightEffect } from "@/components/CardSpotlightEffect";
 
 import Head from "next/head";
 
@@ -13,21 +14,23 @@ export default function Page() {
       <Head>
         <title>Collin Sobowale</title>
         <meta name="description" content="portfolio" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="me.jpeg" />
       </Head>
-      <Header />
-      <Container
-        maxWidth="container.lg"
-        centerContent
-        px={{ base: 4, md: 8, lg: 16 }}
-        pt={{ base: 16, md: 20 }}
-        gap={5}
+      <Click
+        sparkColor="#fff"
+        sparkSize={10}
+        sparkRadius={15}
+        sparkCount={8}
+        duration={400}
       >
-        <AboutMe />
-        <Experience />
-        <Oldexp />
-        <Projects />
-      </Container>
+        <Header />
+
+        <Container maxW="2xl" centerContent px={4} pt={20} gap={5}>
+          <AboutMe />
+          <Experience />
+          <Projects />
+        </Container>
+      </Click>
     </ChakraProvider>
   );
 }
