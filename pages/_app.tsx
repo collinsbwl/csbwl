a1454d5b3c3c8e7f63c4dd2fba36268d17f769eb
Collin.S <collinsobo3@gmail.com>
2025-04-08 18:25:33 -0400

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from "next/app";
 import { Provider } from "@/components/ui/provider";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider>
       <Component {...pageProps} />
+      <Analytics />
     </Provider>
   );
 }
