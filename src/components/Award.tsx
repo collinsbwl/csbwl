7d4c54d0f98b0bfea89860e13bc2271444b9be4f
Collin.S <collinsobo3@gmail.com>
2025-04-13 15:00:52 -0400

    remove award dropdown

--- src/components/Award.tsx
+++ src/components/Award.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Text, Collapsible } from "@chakra-ui/react";
+import { Flex, Box, Text, Heading } from "@chakra-ui/react";
 import { FadeIn } from "animations/fade-in";
 import { FaSortDown } from "react-icons/fa";
 
@@ -6,52 +6,33 @@ const Award: React.FC = () => {
   return (
     <Box w="full">
       <FadeIn delay={1.4}>
-        <Collapsible.Root>
-          <Collapsible.Trigger
-            as={Flex}
-            gap="0.5"
-            fontSize="md"
-            color="grey"
-            py="3"
-            fontWeight="bold"
-          >
-            <Text>Awards</Text>
-            <FaSortDown />
-          </Collapsible.Trigger>
-
-          <Collapsible.Content>
-            <Box paddingLeft="4">
-              <Text fontSize="sm">
-                Continuing Admission Scholarships ($24,000)
-              </Text>
-              <Text fontSize="sm">Distinction Scholarship ($2,500)</Text>
-              <Text fontSize="sm">
-                1st Place - Bank of Montreal Tech Startup Advisory Case
-                Competition
-              </Text>
-              <Text fontSize="sm">
-                Finalist - Deloitte National Strategy & Operations Case
-                Competition
-              </Text>
-              <Text fontSize="sm">
-                Best Mockup - Notion ProdX Case Competition
-              </Text>
-              <Flex justifyContent="center">
-                <Text
-                  fontSize="sm"
-                  textAlign="center"
-                  fontStyle="italic"
-                  color="grey"
-                  pt="20px"
-                  maxW="500px"
-                >
-                  Deeply thankful to everyone who supported me in reaching these
-                  milestones
-                </Text>
-              </Flex>
-            </Box>
-          </Collapsible.Content>
-        </Collapsible.Root>
+        <Heading fontSize="md" color="grey" py="1" fontWeight="bold">
+          Awards
+        </Heading>
+        <Box paddingLeft="4">
+          <Text fontSize="sm">Continuing Admission Scholarships ($24,000)</Text>
+          <Text fontSize="sm">Westerm Distinction Scholarship ($2,500)</Text>
+          <Text fontSize="sm">
+            1st Place - Bank of Montreal Tech Startup Advisory Case Competition
+          </Text>
+          <Text fontSize="sm">
+            Finalist - Deloitte National Strategy & Operations Case Competition
+          </Text>
+          <Text fontSize="sm">Best Mockup - Notion ProdX Case Competition</Text>
+          <Flex justifyContent="center">
+            <Text
+              fontSize="sm"
+              textAlign="center"
+              fontStyle="italic"
+              color="grey"
+              pt="20px"
+              maxW="500px"
+            >
+              Deeply thankful to everyone who supported me in reaching these
+              milestones
+            </Text>
+          </Flex>
+        </Box>
       </FadeIn>
     </Box>
   );
